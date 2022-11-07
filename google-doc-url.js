70aaba556d821fcaeba026a1fbf4e85720d36687
Nathalyapl <117667494+Nathalyapl@users.noreply.github.com>
2022-11-07 17:51:08 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ9bIyf-IMmgv5TnRSqBwZ1JUI6Z5x7sWDSNfrY_BAUttknwoLBU8CMiKzw-GSXtbaC3BMbxWM_Jn_w/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mqpPN7IRFzFSa3CLgLrv_mLoInzVah3vjtgwgB6x8s4/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
